b0c042fccdb5484b04cff3f0d0cf2f6c17789d3f
Larry Kagan <larry@superiocity.com>
2022-04-04 10:55:27 -0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Spatie\RouteDiscovery\Discovery\Discover;
+
+Route::get('/', fn () => view('welcome'));
+
+Discover::views()->in(resource_path('views/discovery'));
